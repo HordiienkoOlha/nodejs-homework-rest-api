52ef80a8888d5d3db738d49a4acc1056d806cb3a
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-08-31 13:15:12 +0300

    edit  controllers and add update favorite

--- middlewares/validationBody.js
+++ middlewares/validationBody.js
@@ -1,17 +1,3 @@
-// const { RequestError } = require("../helpers");
-
-// const validationBody = (schema) => {
-//   const func = async (req, _, next) => {
-//     const { error } = schema.validate(req.body);
-//     if (error) {
-//       next(RequestError(404, error.message));
-//     }
-//     next();
-//   };
-//   return func;
-// };
-
-// module.exports = validationBody;
 const { RequestError } = require("../helpers");
 
 const validationBody = (schema) => {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,24 +4,35 @@ const ctrl = require("../../controllers/contacts");
 
 const { ctrlWrapper } = require("../../helpers");
 
-const { validationBody } = require("../../middlewares");
+const { validationBody, isValidId } = require("../../middlewares");
 
-const { schemas } = require("../../models/contacts");
+const { schemas } = require("../../models/contact");
 const router = express.Router();
 
-// router.get("/", ctrlWrapper(ctrl.getAll));
+router.get("/", ctrlWrapper(ctrl.getAll));
 
-// router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get(
+  "/:contactId",
+  // isValidId,
+  ctrlWrapper(ctrl.getById)
+);
 
-// router.post("/", ctrlWrapper(ctrl.add));
 router.post("/", validationBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 
-// router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
+router.put(
+  "/:contactId",
+  //   isValidId,
+  validationBody(schemas.addSchema),
+  ctrlWrapper(ctrl.updateById)
+);
 
-// router.put(
-//   "/:contactId",
-//   validationBody(schemas.add),
-//   ctrlWrapper(ctrl.updateById)
-// );
+router.patch(
+  "/:contactId/favorite",
+  //   isValidId,
+  validationBody(schemas.updateFavoriteSchema),
+  ctrlWrapper(ctrl.updateFavorite)
+);
+
+router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
 
 module.exports = router;
