84c40c27f69575ef128da951e83739192c8537be
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-09-04 23:17:33 +0300

    add register and dependencies

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const bcrypt = require("bcryptjs");
+
+const { handleSchemaValidationErrors } = require("../helpers");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSchemaValidationErrors);
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const registerSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
